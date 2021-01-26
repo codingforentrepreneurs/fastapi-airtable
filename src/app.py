c82090e02ee678d868cded47bc20e2c512b07cdc
CFE <hello@teamcfe.com>
2021-01-25 22:04:34 -0800

    4 - Rendering Jinja Templates

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent # src
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory = BASE_DIR / "templates" )
 
 # http://localhost:3000/abc # route -> # path
 # https://www.myawesomesite.com/abc
 
 @app.get("/")
-def home_view():
-    return {"hello": "world"}
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
